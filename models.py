dd0ad67bbd7144fb9288acce96129e429f70ef89
ATUL5692 <aksinha1998@gmail.com>
2026-03-12 16:04:41 +0530

    Created schemas for all the tables

---
+++ models.py
@@ -0,0 +1,37 @@
+from pydantic import Base
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key = True, index = True)
+    title = Column(String(200), nullable = False)
+    author= Column(String(200), nullable=False)
+    isbn = Column(String(50), unique = True, nullable=False)
+    category = Column(String(200), nullable=False)
+    total_copies = Column(Integer, nullable=False)
+    available_copies = Column(Integer,default=0)
+    shelf_location = Column(String(200),nullable=False)
+
+class Members(Base):
+    __tablename__= "members"
+
+    member_id = Column(Integer, primary_key= True, index= True,unique=True)
+    name = Column(String(50),nullable=False)
+    email = Column(String(50),unique=True, nullable=False)
+    phone = Column(Integer,unique=True, nullable=False)
+    membership_date = Column(date, nullable=False)
+    status = Column(String, nullable=False)
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    transaction_id = Column(Integer, primary_key=True, index=True)
+    book_id = Column(Integer,ForeignKey("books.id"), nullable=False)
+    member_id = Column(Integer,ForeignKey=("member.member_id"), nullable=False)
+    issue_date = Column(date, nullable=False)
+    due_date = Column(date, nullable=False)
+    return_date = Column(date, nullable=False)
+    status = Column(String, nullable=False)
---
+++ schemas.py
@@ -0,0 +1,70 @@
+from pydantic import BaseModel
+from datetime import date
+
+# Validation for Books
+class BookCreate(BaseModel):
+    title:str
+    author:str
+    isbn:int
+    category:str
+    total_copies:int
+    available_copies:int
+    shelf_location:str
+class BookResponse(BaseModel):
+    id:int
+    title:str
+    author:str
+    isbn:int
+    category:str
+    total_copies:int
+    available_copies:int
+    shelf_location:str
+
+    class config:
+        from_arrributes=True
+
+
+
+
+
+# Validation for Members
+class MembersCreate(BaseModel):
+    name:str
+    email:str
+    phone:int
+    membership_date:date
+    status:str
+
+class MembersResponse(BaseModel):
+    member_id:int
+    name:str
+    email:str
+    phone:int
+    membership_date:date
+    status:str
+
+    class config:
+        from_attributes=True
+
+
+
+
+# Validation for Transactions data
+
+class TransactionsCreate(BaseModel):
+    member_id:int
+    issue_date:date
+    due_date:date
+    return_date:date
+    status:str
+
+class TransactionsResponse(BaseModel):
+    transaction_id:int
+    book_id:int
+    member_id:int
+    issue_date:date
+    due_date:date
+    return_date:date
+    status:str
+    class config:
+        from_attributes=True
