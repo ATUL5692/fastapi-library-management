730787a8cc019bb98969919f535a13fd00274a0a
ATUL5692 <aksinha1998@gmail.com>
2026-03-15 12:16:29 +0530

    Delete book function is added in crud.py file

--- crud.py
+++ crud.py
@@ -1,8 +1,9 @@
-from sqlalchemy.orm import session
-from models import Book, Members, Transaction
-from schemas import BookCreate, MembersCreate
+from sqlalchemy.orm import Session
+import models
+import schemas
 
-def create_book(db: session, book: schemas.BookCreate):
+
+def create_book(db: Session, book: schemas.BookCreate):
     new_book = models.Book(
         title = book.title,
         author = book.author,
@@ -11,12 +12,26 @@ def create_book(db: session, book: schemas.BookCreate):
         total_copies = book.total_copies
     )
 
-    db.add(new_book)
+    db.add(new_book)     
     db.commit()
     db.refresh(new_book)
 
     return new_book
 
 
+def get_books(db: Session):
+    return db.query(models.Book).all()
 
-def get_books()
+def get_book_by_id(db: Session, book_id: int):
+    return db.query(models.Book).filter(models.Book == book_id).first()
+
+def delete_book(db: Session, book_id: int):
+    book = db.query(models.Book).filter(models.Book == Book_id).first()
+
+    if book:
+        db.delete(book)
+        db.commit()
+    
+    return book
+
+def update_book()
